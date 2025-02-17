4c8ddb9e4bc880431f5849920fc3733fde3dfffc
yona-hammouchi <yona.hammouchi@laplateforme.io>
2025-02-17 16:03:29 +0100

    init design and create registerpage

--- pages/inscription.php
+++ pages/inscription.php
@@ -8,26 +8,37 @@ require_once '../class/Database.php'; ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/connexion.css">
+    <link rel="stylesheet" href="../styles/navbar.css">
     <title>Inscription</title>
 </head>
 
 <body>
-    <h1>Inscription</h1>
-    <form action="inscription.php" method="POST">
-        <label for="username">Nom d'utilisateur :</label>
-        <input type="text" id="username" name="username" required><br><br>
+    <?php require_once '../includes/navbar.php'; ?>
+    <section class="containers_head_forms">
+        <section class="container_forms">
+            <h1>Bienvenue au monde, petit trésor !</h1>
+            <p>Inscrivez-vous et partagez un message pour célébrer ce moment spécial.</p>
+            <form action="inscription.php" method="POST">
+                <label for="username">Nom d'utilisateur :</label>
+                <input type="text" id="username" name="username" required><br><br>
 
-        <label for="password">Mot de passe :</label>
-        <input type="password" id="password" name="password" required><br><br>
+                <label for="password">Mot de passe :</label>
+                <input type="password" id="password" name="password" required><br><br>
 
-        <button type="submit">S'inscrire</button>
-    </form>
+                <button type="submit">S'inscrire</button>
+            </form>
 
-    <?php if (isset($result['error'])): ?>
-        <p style="color:red;"><?php echo $result['error']; ?></p>
-    <?php elseif (isset($result['success'])): ?>
-        <p style="color:green;"><?php echo $result['success']; ?></p>
-    <?php endif; ?>
+            <div class="message">
+                <?php if (isset($result['error'])): ?>
+                    <p class="error"><?php echo $result['error']; ?></p>
+                <?php elseif (isset($result['success'])): ?>
+                    <p class="success"><?php echo $result['success']; ?></p>
+                <?php endif; ?>
+            </div>
+        </section>
+    </section>
 </body>
+
 
 </html>
--- pages/login.php
+++ pages/login.php
@@ -1,13 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/navbar.css">
+    <link rel="stylsheet" href="../styles/connexion.css">
+    <title>connexion</title>
+</head>
+
 <body>
+    <?php require_once '../includes/navbar.php'; ?>
+
     <?php if (!empty($error)) : ?>
         <p style="color: red;"><?php echo $error; ?></p>
     <?php endif; ?>
-    <form method="POST" action="">
-        <label for="email">Email de l'utilisateur</label>
-        <input type="email" name="email" id="email" placeholder="Veuillez entrez votre mail" required>
-        <br>
-        <label for="password">Mot de passe de l'utilisateur</label>
-        <input type="password" name="password" id="password" placeholder="Veuillez entre votre mot de passe" required>
-        <br>
-        <button type="submit">Login</button>
-    </form>
+    <section class="containers_head_forms">
+        <section class="container_forms">
+            <h1>Bienvenue au monde, petit trésor !</h1>
+            <form action="inscription.php" method="POST">
+                <label for="username">Nom d'utilisateur :</label>
+                <input type="text" id="username" name="username" required><br><br>
+
+                <label for="password">Mot de passe :</label>
+                <input type="password" id="password" name="password" required><br><br>
+
+                <button type="submit">Se connecter</button>
+            </form>
+
+            <div class="message">
+                <?php if (isset($result['error'])): ?>
+                    <p class="error"><?php echo $result['error']; ?></p>
+                <?php elseif (isset($result['success'])): ?>
+                    <p class="success"><?php echo $result['success']; ?></p>
+                <?php endif; ?>
+            </div>
+        </section>
+    </section>
+</body>
