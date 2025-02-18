6ea0b56fe5faa6b83aef1a0b78555d03515d5233
Your Name <marvin.delansorne@laplateforme.io>
2025-02-18 18:25:00 +0100

    Update navbar

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/navbar.css">
+    <link rel="stylesheet" href="./styles/styleNavbar.css">
     <link rel="stylesheet" href="./styles/style_index.css">
     <title>Livre d'Or</title>
 </head>
