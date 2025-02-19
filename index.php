4871e82b6bc48477161ff25ef0f670c81c223d36
yona-hammouchi <yona.hammouchi@laplateforme.io>
2025-02-19 15:11:50 +0100

    modify style

--- index.php
+++ index.php
@@ -10,83 +10,88 @@
 </head>
 
 <body>
-<header>
-    <nav>
-        <div class="logo">
-            <a href="./index.php"><h1>Au Coeur de la Vie</h1> </a>
-        </div>
-        <ul>
-            <li><a href="./index.php">Acceuil</a></li>
-            <li><a href="./pages/inscription.php">Inscription</a></li>
-            <li><a href="./pages/login.php">Connexion</a></li>
-        </ul>
-    </nav>
-</header>
-<main>
-    <section class="homepage">
-    <img src="./assets/revel.jpg" alt="reveal_img" width="60%">
-    <div class = "paragraphe">
-    <h1>Bienvenue</h1>
-    <p>
-        🌟 Bienvenue au monde, Billy ! 🌟
+    <header>
+        <nav>
+            <div class="logo">
+                <a href="./index.php">
+                    <h1>Au Coeur de la Vie</h1>
+                </a>
+            </div>
+            <ul>
+                <li><a href="./index.php">Acceuil</a></li>
+                <li><a href="./pages/inscription.php">Inscription</a></li>
+                <li><a href="./pages/login.php">Connexion</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="homepage">
+            <img src="./assets/revel.jpg" alt="reveal_img" width="60%">
+            <div class="paragraphe">
+                <h1>Bienvenue</h1>
+                <p>
+                    Bienvenue au monde, Billy !
 
-            C’est avec une immense joie et beaucoup d’émotion que nous vous annonçons la naissance de notre petit trésor, Billy. 
+                    C’est avec une immense joie et beaucoup d’émotion que nous vous annonçons la naissance de notre petit trésor, Billy.
 
-            Nous sommes comblés et impatients de partager avec vous tous les merveilleux moments de cette nouvelle aventure. 
+                    Nous sommes comblés et impatients de partager avec vous tous les merveilleux moments de cette nouvelle aventure.
 
-            Merci à tous ceux qui prendront un moment pour laisser un commentaire bienveillant. 
+                    Merci à tous ceux qui prendront un moment pour laisser un commentaire bienveillant.
 
-            Vos mots chaleureux et vos pensées précieuses nous touchent profondément et rendent ce moment encore plus spécial.
+                    Vos mots chaleureux et vos pensées précieuses nous touchent profondément et rendent ce moment encore plus spécial.
 
-            Nous avons hâte de partager avec vous d’autres moments de bonheur au fur et à mesure que Billy grandit ! 💖
-    </p>
-</div>
-</section>
-<section class="middlepage">
-    <span>
-        <h1>Album photo</h1>
-    </span>
-    <div  class="gallery">
-        <div class="grid">
-            <img src="./assets/babyborn.jpg" alt="img">
-        </div>
-        <div class="grid">
-            <img src="./assets/choes.jpg" alt="img">
-        </div>
-        <div class="grid">
-            <img src="./assets/dadpicture.jpg" alt="img">
-        </div>   
-        <div class="grid">
-            <img src="./assets/grosplanbaby.jpg" alt="img">
-        </div>
-        <div class="grid">
-            <img src="./assets/heart.jpg" alt="img">
-        </div>
-        <div class="grid">
-            <img src="./assets/img.jpg" alt="img">
-        </div>
-        <div class="grid">
-            <img src="./assets/pictureone.jpg" alt="img">
-        </div>
-        <div class="grid">
-            <img src="./assets/picturethree.jpg" alt="img">
-        </div>
-        <div class="grid">
-            <img src="./assets/picturetwo.jpg" alt="img">
-        </div>
-        <div class="grid">
-            <img src="./assets/start.jpg" alt="img">
-        </div>
-    </div>
-
-</section>
-    <span>
-        <h1> N'hésitez pas à nous laissez un petit message sur le livre d'or </h1>
-    <span>
-    <a href="./pages/inscription.php"><img class = "register_button"src="./assets/button_register.png" alt="img"></a>
-</main>
-<footer>
-</footer>
+                    Nous avons hâte de partager avec vous d’autres moments de bonheur au fur et à mesure que Billy grandit !
+                </p>
+            </div>
+        </section>
+        <section class="container_button_book">
+            <p>Voici notre
+                <a href="../pages/inscription.php">
+                    <button>Livre d'or</button>
+                </a>
+                où vous pouvez nous laisser un petit message.
+            </p>
+        </section>
+        <section class="middlepage">
+            <span>
+                <h1>Album photo</h1>
+            </span>
+            <div class="gallery">
+                <div class="grid">
+                    <img src="./assets/babyborn.jpg" alt="img">
+                </div>
+                <div class="grid">
+                    <img src="./assets/choes.jpg" alt="img">
+                </div>
+                <div class="grid">
+                    <img src="./assets/dadpicture.jpg" alt="img">
+                </div>
+                <div class="grid">
+                    <img src="./assets/grosplanbaby.jpg" alt="img">
+                </div>
+                <div class="grid">
+                    <img src="./assets/heart.jpg" alt="img">
+                </div>
+                <div class="grid">
+                    <img src="./assets/img.jpg" alt="img">
+                </div>
+                <div class="grid">
+                    <img src="./assets/pictureone.jpg" alt="img">
+                </div>
+                <div class="grid">
+                    <img src="./assets/picturethree.jpg" alt="img">
+                </div>
+                <div class="grid">
+                    <img src="./assets/picturetwo.jpg" alt="img">
+                </div>
+                <div class="grid">
+                    <img src="./assets/start.jpg" alt="img">
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer>
+    </footer>
 </body>
 
 </html>
